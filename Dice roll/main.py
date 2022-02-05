ce0cd93ab06f60dd6651f3f40992049c1a69d202
Temi <stadeniy@cougarnet.uh.edu>
2022-02-05 11:04:54 -0600

    Dice completed, I was tired and sleep last night solved it while taking a shower.

--- Dice roll/main.py
+++ Dice roll/main.py
@@ -1,18 +1,23 @@
 import random
+from turtle import goto
 
 min =1
 max =6
+prev_roll=[]
 
 
-
-def Roll(value, tries):
+def Roll(value):
     got = random.randint(min, max)
-    while tries != 0:
-        tries=-1
-        if got == value:
-            return got
-    print(got)
+    while value != got:
+        prev_roll.append(got)
+        got = random.randint(min, max)
+    return got
 
+def print_prev_rolls():
+    for i in range(len(prev_roll)):
+        print(prev_roll[i])
 
-ans = Roll(4, 10)
+ans = Roll(4)
 print("the Answer is: ", ans)
+print_prev_rolls()
+print(*prev_roll)
